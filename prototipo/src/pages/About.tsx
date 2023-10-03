dd655ca0dd44aa1b37792a19bfaf4e56208e25cf
Agustin Ghent <agustineghent@gmail.com>
2023-10-03 12:48:40 -0300

    react boiler plate ready

---
+++ prototipo/src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExampleComponent from '../components/ExampleComponent'; // Adjust the import path as needed
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <ExampleComponent />
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default About;
